2f75fa060bbd3f9b482c5f68253236167bc6e191
Nebiyu <nebiyu28@gmail.com>
2023-01-07 02:33:31 +0300

    maximum icecream

---
+++ Easy/maxIceCream/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(maxIceCream(new int[]{1,3,2,4,1}, 7));
+    }
+    public static int maxIceCream(int[] costs, int coins){
+        Arrays.sort(costs);
+        int n = costs.length;
+        int answer = 0;
+        while (answer < costs.length && costs[answer] < coins){
+            coins -=costs[answer];
+            answer++;
+        }
+
+return answer;
+    }
+}
