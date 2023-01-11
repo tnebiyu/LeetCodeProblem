0d448a5ca70f36a7dd11ba43cfc78a63efd5906f
Nebiyu <nebiyu28@gmail.com>
2023-01-11 10:10:59 +0300

    arrasy.tostring

--- Easy/Insertion Sort/src/Main.java
+++ Easy/Insertion Sort/src/Main.java
@@ -1,18 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-for(int firstUnsortedIndex =1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++){
-    int newElement = intArray[firstUnsortedIndex];
-    int i;
-    for (i= firstUnsortedIndex; i>0 && intArray[i-1]> newElement; i--){
-        intArray[i] = intArray[i-1];
-    }
-    intArray[i] = newElement;
-}
-for (int i=0; i<intArray.length; i++){
-    System.out.println(intArray[i]);
-}
+        int[] intArray = {2,12,3,4,5,1,82,23};
+        for (int unsorted = 1; unsorted < intArray.length; unsorted++){
+            int newElement = intArray[unsorted];
+            int i;
+            for (i = unsorted; i>0 && intArray[i-1] > newElement; i--){
+                intArray[i] = intArray[i-1];
 
+            }
+            intArray[i] = newElement;
 
+        }
+            System.out.println("sorted array become " + Arrays.toString(intArray));
     }
 }
